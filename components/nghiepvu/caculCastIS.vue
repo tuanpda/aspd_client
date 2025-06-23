aa065b8241e4c4fbe5a45e4d2dab2eada7e86d75
admin.hatinh <admin.hatinh@vss.local>
2025-06-23 10:25:29 +0700

    commit

--- components/nghiepvu/caculCastIS.vue
+++ components/nghiepvu/caculCastIS.vue
@@ -3003,7 +3003,7 @@ export default {
       }
     },
 
-    tinhTienPhaiDongXXX(madoituong, muctiendong, maphuongthucdong, tuthang) {
+    tinhTienPhaiDong(madoituong, muctiendong, maphuongthucdong, tuthang) {
       // console.log("Từ tháng đã nhập vào:", tuthang);
       const denthang = this.tinhDenThang(tuthang, maphuongthucdong);
       // console.log("Đến tháng:", denthang);
@@ -3035,7 +3035,7 @@ export default {
     },
 
     // tinhTienPhaiDong(madoituong, muctiendong, maphuongthucdong, tuthang, dadongdenthang)
-    tinhTienPhaiDong(
+    tinhTienPhaiDongXXX(
       madoituong,
       muctiendong,
       maphuongthucdong,
