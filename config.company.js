3ffdcab42d2e3785701951d826bd498c9d0a7f38
tuanpda <tuanpda.bhxh@gmail.com>
2025-07-02 18:08:48 +0700

    commit

--- config.company.js
+++ config.company.js
@@ -3,12 +3,14 @@ export default {
   companyName: "CÔNG TY TNHH AN SINH PHỦ DIỄN",
   companyNameThuong: "Công ty TNHH An sinh Phủ Diễn",
   bhxhName: "BẢO HIỂM XÃ HỘI LIÊN HUYỆN DIỄN CHÂU - NGHI LỘC",
-  apiBaseURL: "http://14.224.148.17:1552", // CỔNG 4019 LÀ BACKEND CHO HƯNG NGUYÊN
-  clientURL: "http://14.224.148.17:4042", // CLIENT
+  // apiBaseURL: "http://14.224.148.17:1552", //
+  apiBaseURL: "http://ansinhxahoiphudien.vn:1552", //
+  // clientURL: "http://14.224.148.17:4042", // CLIENT
+  clientURL: "http://ansinhxahoiphudien.vn/",
   port_client: 3000, // CỔNG CLIENT HƯNG NGUYÊN
   port_backend: 1552,
   host: "192.168.1.5", // MÁY CHỦ CHUNG TRÊN HỆ THỐNG PHỦ DIỄN
   namebienlai: "ansinhphudien",
-  urlBienlaidientu: `http://14.224.148.17:4042/tracuubienlaidientu-ansinhphudien`,
+  urlBienlaidientu: `http://ansinhxahoiphudien.vn/tracuubienlaidientu-ansinhphudien`,
   dmbenhvien: "dienchau", // MÃ ĐỊNH DANH BỆNH VIỆN
 };
